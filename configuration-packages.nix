2ce8f09cb032aac8b17caf1f840886f8cedc9333
Shpinog One <shpinog@gmail.com>
2020-09-22 17:20:55 +0300

    2

--- configuration-packages.nix
+++ configuration-packages.nix
@@ -3,40 +3,18 @@
 {
   environment.systemPackages =
   with pkgs; [
-#    (import ./hello2)
-    (callPackage ./hello2 {})
     sudo
     wget
-    connman
-    connman-gtk
-    wpa_supplicant_gui
-#    networkmanager
-#    iwd
-
-    mkpasswd
     unzip unar
-    emacs git
-    escrotum feh
-    vscode-with-extensions
+    git
+    vscode-with-extensions vim
     vlc
-    docker docker-compose
-    google-chrome tor-browser-bundle-bin
-    termite dmenu
-    ocaml opam gnumake gcc gnum4 binutils-unwrapped dune pkg-config ocamlformat
-    ghc cabal-install stack cabal2nix nix-prefetch-git
-
-    xmonad-with-packages
-    pkgs.haskellPackages.xmonad
-    pkgs.haskellPackages.xmonad-extras
-    pkgs.haskellPackages.xmonad-contrib
-    xmobar
-
-    nodejs yarn nodePackages.node2nix
+    tor-browser-bundle-bin tdesktop firefox
+    volctl flameshot steam-run mesa htop
+    dmenu alacritty
+    gnumake gcc
     python
-    ninja
     utillinux
-    brave
-    xorg.xmessage
 
 #    bluez blueman
   ];
